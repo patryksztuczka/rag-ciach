6f84239d5cdb75a34779992a9e11ff9376e08531
Patryk Sztuczka <73931963+patryksztuczka@users.noreply.github.com>
2025-07-16 19:26:38 +0200

    feat: Implement txt and md file upload, gh actions setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default tseslint.config([
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+    ],
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+      prettier: eslintPluginPrettier,
+    },
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+]);
